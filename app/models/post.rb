dfab647a0c6803a914a5a3e3823d34d2c45984b1
Mike <m@thecoded.com>
2014-06-27 17:01:14 -0400

    installed bootstrap

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post
   include Mongoid::Document
   field :Title, type: String
   field :Body, type: String
-  field :created_at, type:Date
+  field :created_at, type:Date,  default: ->{ Time.now }
 end
